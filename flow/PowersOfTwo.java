f0d94c03f5b75cb8c748b162136c8fa99480ae64
Martin Tan <Martin-Tan@users.noreply.github.com>
2018-04-26 18:25:13 +0800

    Add update comment

--- flow/PowersOfTwo.java
+++ flow/PowersOfTwo.java
@@ -1,12 +1,17 @@
 /******************************************************************************
  *  Compilation:  javac PowersOfTwo.java
- *  Execution:    java PowersOfTwo x
+ *  Execution:    java PowersOfTwo n
  *  
  *  This is program takes a command-line argument n and prints a table of
  *  the powers of 2 that are less than or equal to 2^n
  * 
  *  % java PowersOfTwo 5
- *  
+ *  2 to the power of 0 is 1
+ *  2 to the power of 1 is 2
+ *  2 to the power of 2 is 4
+ *  2 to the power of 3 is 8
+ *  2 to the power of 4 is 16
+ *  2 to the power of 5 is 32
  *
  ******************************************************************************/
 
